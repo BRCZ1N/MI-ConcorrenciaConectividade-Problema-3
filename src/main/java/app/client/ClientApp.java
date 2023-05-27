81d9adaff0ffec3a11df366721685804f82bda24
xXgueloseXx <88115079+Oguelo@users.noreply.github.com>
2023-05-26 23:42:10 -0300

    update client

--- src/main/java/app/client/ClientApp.java
+++ src/main/java/app/client/ClientApp.java
@@ -1,6 +1,12 @@
 package app.client;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import app.model.AccountModel;
+import app.model.BankModel;
+import app.utilities.BanksEnum;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,9 +24,9 @@ public class ClientApp {
 	private boolean log = true;
 	private boolean pick = true;
 	private Scanner scanner = new Scanner(System.in);
-	private String banco;
 	private String escolha;
-	private volatile String bankIp;
+	private volatile BankModel bankIp;
+	private AccountModel user;
 	private void execBank() throws IOException, UnableToConnectException {
 		pickBank();
 		loginClient();
@@ -29,7 +35,7 @@ public class ClientApp {
 	}
 
 	private void pickBank() {
-		while(pick) {
+		while (pick) {
 			System.out.println("===================================================");
 			System.out.println("========= Bancos Disponiveis ==========");
 			System.out.println("===================================================");
@@ -38,61 +44,97 @@ public class ClientApp {
 			System.out.println("====== (2) - Banco 2");
 			System.out.println("====== (3) - Banco 3");
 			System.out.println("====== (4) - Banco 4");
-			banco = scanner.next();
+			String banco = scanner.next();
 			if (banco.equals("1") || banco.equals("2") || banco.equals("3") || banco.equals("4")) {
-				while(pick){
+				while (pick) {
 					escolha = changeGo(banco);
-					if(escolha.equals("1")) {
-						 
-						 if(banco.equals("1")) {
-								
-								bankIp = ConfigLarsidIpsHttp.HTTP_FOG_REGION_Q1.getAddress();
-							}else if(banco.equals("2")) {
-								
-								bankIp = ConfigLarsidIpsHttp.HTTP_FOG_REGION_Q2.getAddress();
-							}else if(banco.equals("3")) {
-								
-								bankIp = ConfigLarsidIpsHttp.HTTP_FOG_REGION_Q3.getAddress();
-							}else if(banco.equals("4")) {
-								
-								bankIp = ConfigLarsidIpsHttp.HTTP_FOG_REGION_Q4.getAddress();
-							}
-						 pick = false;
-					}else if(escolha.equals("2")) {
+					if (escolha.equals("1")) {
+
+						BankModel ipBank = setIpBank(banco);
+						pick = false;
+					} else if (escolha.equals("2")) {
 						System.out.println("Escolha outro banco");
-					}else {
+
+					} else if (escolha.equals("3")) {
+						BankModel ipBank = setIpBank(banco);
+						registerUser(ipBank);
+
+					} else {
 						System.out.println("Digite uma opção correta");
 						escolha = changeGo(banco);
 					}
-		           
+
 				}
-	            
-	        } else {
-	            System.out.println("Banco invalido");
-	        }
+
+			} else {
+				System.out.println("Banco invalido");
+			}
 		}
+
+	}
+
+	private BankModel setIpBank(String banco) {
+		if (banco.equals("1")) {
+
+			bankIp = BanksEnum.BANK_1.getBank();
+		} else if (banco.equals("2")) {
+
+			bankIp = BanksEnum.BANK_2.getBank();
+		} else if (banco.equals("3")) {
+
+			bankIp = BanksEnum.BANK_3.getBank();
+		} else if (banco.equals("4")) {
+
+			bankIp = BanksEnum.BANK_4.getBank();
+		}
+		return bankIp;
 		
 	}
 
-	private String changeGo(String banco) {
+	/**
+	 * @param ipBank
+	 */
+	private void registerUser(BankModel ipBank) {
 		
-		System.out.println("Banco selecionado: " + "BANCO"+""+banco);
-        System.out.println("========= Pagina Inicial ==========" );
-        System.out.println("===================================================");
-        System.out.println("====== (1) - Tela de Login");
+		System.out.println("====== Digite a senha desejada: ======");
+		String senhaRegister = scanner.next();
+		System.out.println("====== Deseja Cadastrar quantos beneficiarios: ======");
+		int countBeneficiares = scanner.nextInt();
+		int count = 0;
+		if (countBeneficiares > 0) {
+			while(countBeneficiares != count) {
+				System.out.println("====== Digite o nome do beneficiario: ======");
+				String beneficiareName = scanner.next();
+				System.out.println("====== Digite o cpf do beneficiario: ======");
+				String beneficiareCpf = scanner.next();
+				
+				count += 1;
+			}
+		}else {
+			user = new AccountModel(senhaRegister, ipBank , null);
+		}
+	}
+
+	private String changeGo(String banco) {
+
+		System.out.println("Banco selecionado: " + "BANCO" + "" + banco);
+		System.out.println("========= Pagina Inicial ==========");
+		System.out.println("===================================================");
+		System.out.println("====== (1) - Tela de Login");
 		System.out.println("====== (2) - Alterar banco");
+		System.out.println("====== (3) - Criar conta");
 		System.out.println("===================================================");
 		String escolha = scanner.next();
 		return escolha;
-		
+
 	}
 
 	private void loginClient() throws IOException {
 		while (log) {
 			System.out.println("===================================================");
-			System.out.println("========= Central bancaria:"+ banco +" ==========");
+			System.out.println("========= Central bancaria: ==========");
 			System.out.println("===================================================");
-			System.out.println("====== Digite seu cpf: ======");
+			System.out.println("====== Digite seu id: ======");
 			String cpf = scanner.next();
 			System.out.println("====== Digite sua senha: ======");
 			String senha = scanner.next();
@@ -122,11 +164,12 @@ public class ClientApp {
 	public ResponseHttp messageReturn(String method, String endpoint, String httpVersion, Map<String, String> header,
 			JSONObject json) throws IOException {
 
-		ResponseHttp response = Http.sendHTTPRequestAndGetHttpResponse(
-				new RequestHttp(method, endpoint, httpVersion, header),json);
+		ResponseHttp response = Http
+				.sendHTTPRequestAndGetHttpResponse(new RequestHttp(method, endpoint, httpVersion, header), json);
 		return response;
 
 	}
+
 	public static void main(String[] args) throws IOException, UnableToConnectException {
 
 		ClientApp appBank = new ClientApp();
